12fa68827fadb225202eea6cdb1e309c25716b75
Boriz81 <blitzboriz@gmail.com>
2023-01-09 20:09:48 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D example = new Point2D();
+        example.setX(10);
+        example.setY(15);
+        System.out.println(example.getX());
+        System.out.println(example.getY());
+    }
+}
---
+++ src/Point2D.java
@@ -0,0 +1,22 @@
+public class Point2D {
+    float x;
+    float y;
+
+    public float getX() {
+        return this.x;
+
+    }
+    public void setX(float x) {
+        this.x = x;
+
+    }
+
+    public float getY() {
+        return this.y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+
+    }
+}
